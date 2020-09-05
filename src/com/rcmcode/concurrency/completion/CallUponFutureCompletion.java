c0396668b95d3f404a7d29c3c264b72a51807f90
mohanrajrc <mohanrajksrct@gmail.com>
2020-09-05 10:47:24 +0530

    base-package-refactor

---
+++ src/com/rcmcode/concurrency/completion/CallUponFutureCompletion.java
@@ -0,0 +1,71 @@
+package com.rcmcode.concurrency.completion;
+
+import com.rcmcode.concurrency.LongTask;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CallUponFutureCompletion {
+
+    public static void main(String[] args) {
+        //thenRunBehavior();
+        //thenRunAsyncBehavior();
+        //thenAcceptBehavior();
+        thenAcceptAsyncBehavior();
+    }
+
+    private static void thenRunBehavior() {
+        CompletableFuture future = CompletableFuture.supplyAsync(() -> {
+            LongTask.simulate(2000);
+            System.out.println("Async call is processed by:"+Thread.currentThread().getName());
+            return 1;
+        });
+        future.thenRun(()-> {
+            System.out.println("Successfully completed Async call in:"+Thread.currentThread().getName());
+        });
+        System.out.println("Waiting for the result");
+        LongTask.simulate(3000);
+    }
+
+    private static void thenRunAsyncBehavior() {
+        CompletableFuture future = CompletableFuture.supplyAsync(() -> {
+            //LongTask.simulate(2000);
+            System.out.println("Async call is processed by:"+Thread.currentThread().getName());
+            return 1;
+        });
+        future.thenRunAsync(()-> {
+            System.out.println("Successfully completed Async call in:"+Thread.currentThread().getName());
+        });
+        System.out.println("Waiting for the result");
+        LongTask.simulate(3000);
+    }
+
+    private static void thenAcceptBehavior() {
+        CompletableFuture future = CompletableFuture.supplyAsync(() -> {
+            //LongTask.simulate(2000);
+            System.out.println("Async call is processed by:"+Thread.currentThread().getName());
+            return 1;
+        });
+        future.thenAccept(result-> {
+            System.out.println("Successfully completed Async call in:"+Thread.currentThread().getName());
+            System.out.println("result:"+result);
+        });
+        System.out.println("Waiting for the result");
+        LongTask.simulate(3000);
+    }
+
+    private static void thenAcceptAsyncBehavior() {
+        CompletableFuture future = CompletableFuture.supplyAsync(() -> {
+            //LongTask.simulate(2000);
+            System.out.println("Async call is processed by:"+Thread.currentThread().getName());
+            return 1;
+        });
+        future.thenAcceptAsync(result-> {
+            System.out.println("Successfully completed Async call in:"+Thread.currentThread().getName());
+            System.out.println("result:"+result);
+
+        });
+        System.out.println("Waiting for the result");
+        LongTask.simulate(3000);
+    }
+
+}
---
+++ src/com/rcmcode/concurrency/transform/CompletableFutureTransformDemo.java
@@ -0,0 +1,21 @@
+package com.rcmcode.concurrency.transform;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureTransformDemo {
+
+    public static void main(String[] args) {
+        transformFuture();
+    }
+
+    public static void transformFuture() {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> 30);
+        future
+            .thenApply(CompletableFutureTransformDemo::toFahrenheit)
+            .thenAccept(System.out::println);
+    }
+
+    private static double toFahrenheit(double celsius) {
+        return (celsius * 1.8) + 32;
+    }
+}
